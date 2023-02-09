45762ef8557d3bd29db4ea3204041b2cdf93e12e
hyukzz <ri_chard@naver.com>
2023-02-09 22:02:53 +0900

    Feat: container routing 세팅

---
+++ components/container/Container.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import { ContainerContainer, Main } from './Container.style';
+// import Footer from './Footer';
+// import GA from './GA';
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('@/components/header/Header'), {
+  ssr: false,
+});
+
+const Container = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content='width=device-width, initial-scale=1' name='viewport' />
+      </Head>
+      {/* <GA /> */}
+      <ContainerContainer>
+        <Header />
+        <Main>{children}</Main>
+        {/* <Footer /> */}
+      </ContainerContainer>
+    </>
+  );
+};
+
+export default Container;
