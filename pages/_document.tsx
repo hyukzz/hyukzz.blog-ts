ce3d52966fe7954226553f3f46d20783f100095f
hyukzz <ri_chard@naver.com>
2023-02-10 00:46:11 +0900

    Fix: 첫 랜더링 시 gif가 랜더링 안되는 버그 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
+function Document() {
+  const setThemeMode = `
+    function getThemeMode() {
+        const theme = window.localStorage.getItem('theme')
+        return theme ? theme : 'dark'
+    }
+    document.body.dataset.theme = getThemeMode()
+  `;
+
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='ko'>
+      <Head>
+        <link href='' rel='shortcut icon' />
+      </Head>
       <body>
+        <script dangerouslySetInnerHTML={{ __html: setThemeMode }} />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+export default Document;
