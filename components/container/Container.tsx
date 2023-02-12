4880615369d560d5deb4901102b6dd916ebf512c
hyukzz <ri_chard@naver.com>
2023-02-12 21:48:44 +0900

    Merge: 파일명 변경사항

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -1,11 +1,7 @@
 import Head from 'next/head';
 import { ContainerContainer, Main } from './Container.style';
 import Footer from '@/components/footer/Footer';
-<<<<<<< HEAD
 import GoogleAnalytics from '@/components/google/GoogleAnalytics';
-=======
-// import GA from './GA';
->>>>>>> d9582d58435ffb5d43aed8a5376cdac11056f8fd
 import dynamic from 'next/dynamic';
 
 const Header = dynamic(() => import('@/components/header/Header'), {
--- components/google/GoogleAnalytics.tsx
+++ components/google/GoogleAnalytics.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 import metadata from '@/data/metadata';
 
-const GA = () => {
+const GoogleAnalytics = () => {
   return (
     <>
       <Script
@@ -21,4 +21,4 @@ const GA = () => {
   );
 };
 
-export default GA;
+export default GoogleAnalytics;
