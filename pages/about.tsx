b9212c61cb37a83f9a09ecdee565d1c08c5ee193
hyukzz <ri_chard@naver.com>
2023-02-13 21:51:01 +0900

    Feat: /about 컴포넌트 제작

---
+++ pages/about.tsx
@@ -0,0 +1,21 @@
+import Title from '@/components/title/Title';
+import AboutProfile from '@/components/about/AboutProfile';
+import Container from '@/components/container/Container';
+import metadata from '@/data/metadata';
+import { NextSeo } from 'next-seo';
+
+function Aboutpage() {
+  return (
+    <Container>
+      <NextSeo
+        title='About'
+        canonical={`${metadata.meta.url}/about`}
+        openGraph={{ url: `${metadata.meta.url}/about` }}
+      />
+      <Title title='About' des='Let me introduce myself 🌈' />
+      <AboutProfile />
+    </Container>
+  );
+}
+
+export default Aboutpage;
