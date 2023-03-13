85e8bfb56d8f4c63a091f35ea487478f06a68d7a
hyukzz <ri_chard@naver.com>
2023-03-13 19:24:18 +0900

    feat: nav projects 데이터

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,6 +1,6 @@
 const navLinks: { title: string; path: string; link: string }[] = [
   { title: 'Home', path: '/', link: '/' },
-  // { title: 'Projects', path: '/projects', link: '/projects' },
+  { title: 'Project', path: '/projects', link: '/projects' },
   { title: 'About', path: '/about', link: '/about' },
   { title: 'Resume', path: '/resume', link: '/resume' },
 ];
