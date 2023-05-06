2bf2ed9db27b133362e5fbde914b4a22c0476feb
hyukzz <ri_chard@naver.com>
2023-05-06 22:00:34 +0900

    update: dynamic header

--- components/container/Container.tsx
+++ components/container/Container.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import { ContainerContainer, Main } from './Container.style';
 import Footer from '@/components/footer/Footer';
 import GoogleAnalytics from '@/components/google/GoogleAnalytics';
-import Header from '@/components/header/Header';
+
+const Header = dynamic(() => import('@/components/header/Header'), {
+  ssr: false,
+});
 
 const Container = ({ children }) => {
   return (
